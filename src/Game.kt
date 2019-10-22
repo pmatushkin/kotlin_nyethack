c775ef21ff9040712fb4b390e042bc49e7f72888
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-21 19:21:45 -0700

    feat: Add nested if/else condition

--- src/Game.kt
+++ src/Game.kt
@@ -1,13 +1,17 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 89
+    val isBlessed = true
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition!")
     } else if (healthPoints >= 90) {
         println(name + " has a few scratches.")
     } else if (healthPoints >= 75) {
-        println(name + " has some minor wounds.")
+        if (isBlessed)
+            println(name + " has some minor wounds but is healing quite quickly!")
+        else
+            println(name + " has some minor wounds.")
     } else if (healthPoints >= 15) {
         println(name + " looks pretty hurt.")
     } else {

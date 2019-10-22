0a2411757f7b351ff3a29b28aed23b900b0ffbb4
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-21 19:28:14 -0700

    feat: Use logical operators in the declaration of a variable

--- src/Game.kt
+++ src/Game.kt
@@ -5,7 +5,8 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
     // Aura
-    if (isBlessed && (healthPoints > 50) || isImmortal) {
+    val auraVisible = isBlessed && (healthPoints > 50) || isImmortal
+    if (auraVisible) {
         println("GREEN")
     } else {
         println("NONE")

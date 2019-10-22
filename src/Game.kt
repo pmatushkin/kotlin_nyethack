bfb662228611b13481e6ecf2809ac80cf010ae87
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-21 19:15:53 -0700

    feat: Implement simple if/else condition

---
+++ src/Game.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    var healthPoints = 100
+
+    if (healthPoints == 100) {
+        println(name + " is in excellent condition!")
+    } else {
+        println(name + " is in awful condition!")
+    }
+}

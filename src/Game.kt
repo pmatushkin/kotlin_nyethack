a0bbf689619affbf1f89a724a854ab73f27876cb
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-21 19:26:14 -0700

    feat: Add if/else condition with logical operator

--- src/Game.kt
+++ src/Game.kt
@@ -2,6 +2,14 @@ fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 89
     val isBlessed = true
+    val isImmortal = false
+
+    // Aura
+    if (isBlessed && (healthPoints > 50) || isImmortal) {
+        println("GREEN")
+    } else {
+        println("NONE")
+    }
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition!")

e2d178785df1bd1666afc14e61e4692c342982bd
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-21 19:19:10 -0700

    feat: Add more simple if/else conditions

--- src/Game.kt
+++ src/Game.kt
@@ -1,9 +1,15 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 100
+    var healthPoints = 89
 
     if (healthPoints == 100) {
         println(name + " is in excellent condition!")
+    } else if (healthPoints >= 90) {
+        println(name + " has a few scratches.")
+    } else if (healthPoints >= 75) {
+        println(name + " has some minor wounds.")
+    } else if (healthPoints >= 15) {
+        println(name + " looks pretty hurt.")
     } else {
         println(name + " is in awful condition!")
     }

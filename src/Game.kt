a7662bca77bd09d39fb66fbc3f6a0f7286b9d23c
Pavel Matushkin <pmatushkin@gmail.com>
2019-10-23 19:22:17 -0700

    feat: Use single-expression functions

--- src/Game.kt
+++ src/Game.kt
@@ -15,9 +15,8 @@ fun main(args: Array<String>) {
     castFireball()
 }
 
-fun castFireball(numFireballs: Int = 2) {
+fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
-}
 
 private fun printPlayerStatus(
     auraColor: String,
@@ -35,13 +34,11 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     return auraColor
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) "has some minor wounds but is healing quite quickly!" else "has some minor wounds."
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    return healthStatus
-}
